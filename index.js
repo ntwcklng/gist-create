6a4b066efa14e7c806cec6d90bd7cb96f2489925
ntwcklng <ntwcklng@gmail.com>
2016-08-14 09:04:50 +0200

    :boom: eslint + better default opts

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const githubClient = require('github');
+const GithubClient = require('github');
 const fs = require('fs');
 const chalk = require('chalk');
 const copyToClipboard = require('copy-paste');
 
-const github = new githubClient({
-  version: "3.0.0"
+const github = new GithubClient({
+  version: '3.0.0',
 });
 
 
-module.exports = function(file, options) {
-  const public = options.public || false;
-  const description = options.description || 'Created with https://www.npmjs.com/package/gist-create';
+module.exports = function gistCreate(file, options) {
   const fileContent = fs.readFileSync(file, 'utf-8', (err, data) => {
-    if(err) console.log(chalk.red(`Make sure your ${file} exists. \n ${err}`));
+    if (err) console.error(chalk.red(`Make sure your ${file} exists. \n ${err}`)); // eslint-disable-line
     return data;
   });
 
-  const files = {};
-  files[file] = {
-    'content': fileContent
-  }
-  const createObj = {
-    "description": description,
-    "public": public,
-    "files": files
-  }
+  const filesObj = {};
+  filesObj[file] = {
+    content: fileContent,
+  };
+  const defaultOptions = {
+    description: 'Created with https://www.npmjs.com/package/gist-create',
+    public: false,
+    files: filesObj,
+  };
+
+  const createObj = Object.assign(defaultOptions, options);
   github.gists.create(createObj, (err, res) => {
-    if(err) console.log(chalk.red(err));
-    copyToClipboard.copy(res.html_url, () => {
+    if (err) console.log(chalk.red(err));
+    copyToClipboard.copy(res.html_url, () => { // eslint-disable-line
       return console.log(chalk.blue(`Copied ${res.html_url} to clipboard!`));
     });
   });
-}
+};
